1bb8eb952628bf48a7fb523f620aa79185dac708
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-04 13:21:31 +0530

    create authentication

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import { use } from "react";
+import config from "../config/config";
+import { Client, Account, ID } from "appwrite";
+export class AuthService {
+    client = new Client();
+    account;
+    
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectID);
+        this.account = new Account(this.client);
+
+    }
+    async CreateAccount({ email, password, name }) {
+
+        try {
+            const useAccount = await this.account.create(ID.unique(), email, password, name)
+            if (useAccount) {
+                // call another method
+                return this.login({ email, password })
+            }
+            else {
+                return useAccount;
+            }
+
+
+        } catch (error) {
+            throw error;
+        }
+
+
+    }
+    async login({ email, password }) {
+        try {
+            await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error
+        }
+    }
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite services::getCurrentUser :: error", error)
+        }
+        return null;
+    }
+    async logOut() {
+        try {
+            await this.account.deleteSession();
+        } catch (error) {
+            console.log("AppWrite servics::logout::error",error)
+        }
+    }
+}
+const AuthService = new AuthService()
+export default AuthService;
