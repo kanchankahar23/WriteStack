8cf9c08ed70e04d9085212a5fc17054f5a73e448
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-04 16:11:07 +0530

    create authentication

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -5,7 +5,7 @@ export class Services {
     client = new Client();
     databases;
     bucket;
-    
+
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
@@ -13,6 +13,50 @@ export class Services {
         this.databases = new Databases(this.client);
         this.bucket = new Storage(this.client);
 
+    }
+    async createpost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+
+            console.log("Appwrite service :: createPost :: error", error)
+        }
+    }
+    async updatepost(slug, { title, content, featuredImage, status }){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("AppWrite service :: updatepost :: error", error)
+        }
+    }
+    async deletepost(slug){
+        try {
+            await this.databases.deleteDocument
+            
+        } catch (error) {
+            console.log("Appwrite service :: updatepost :: error",error);
+        }
     }
 }
 
