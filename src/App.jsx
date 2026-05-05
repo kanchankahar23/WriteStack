8eb5dc3fc5bc99a56b4ec5b0ac937e47d748c530
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-05 12:32:13 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from "./store/authStore"
+import  Header  from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import { Outlet } from 'react-router-dom'
 const App = () => {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <div className='text-3xl text-white text-center'>App</div>
-  )
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex bg-gray-400 justify-center items-center flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        Todo:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
