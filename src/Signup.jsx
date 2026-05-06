4266b296bb44fe351e20e9811d6e92d10cccdaa6
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-06 12:01:01 +0530

    update

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import authService from './appwrite/auth'
+import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { login } from './store/authStore'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div>Signup</div>
+    )
 }
 
 export default Signup
