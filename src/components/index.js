25d620dc72781d8ec9e3d7f93800797698ecc3d9
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-15 20:24:43 +0530

    refactor: add missing component imports in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,30 @@
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTA from "./RTA";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./postform/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    LogoutBtn,
+    RTA,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
