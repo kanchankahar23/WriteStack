f421674dd03e77b07839611dd550640c1342cd89
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-15 14:05:25 +0530

    Create Feature

---
+++ src/components/postform/PostForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import {Button,Input,Select,RTA}  from "../index"
+import appwriteService from "../../appwrite/config"
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
