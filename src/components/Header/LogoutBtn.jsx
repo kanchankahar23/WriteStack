5b31bc0da5e4dad80d4b40bc6e1d0c1d7d8252ee
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-15 20:28:11 +0530

    fix: update import path for logout action in LogoutBtn component

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from '../../appwrite/config'
-import { logout } from '../../store/authStore'
+import { logout } from '../../store/authSlice'
 
 const LogoutBtn = () => {
     const dispatch = useDispatch()
