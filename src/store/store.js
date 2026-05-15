244c5782e2dddf2deb828c10185c8bb9bc56122c
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-15 20:30:36 +0530

    feat: configure Redux store with authSlice for user authentication

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice"; // ✅ import karo
+
 const store = configureStore({
-    reducer:{}
+  reducer: {
+    auth: authSlice, // ✅ register karo
+  },
 });
 
 export default store;
