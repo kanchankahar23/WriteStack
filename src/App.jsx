8efc033ca868fdfc53e58d73af260a248e40111e
kanchan kahar <kaharkanchan884@gmail.com>
2026-05-15 20:22:41 +0530

    fix: update import path for auth actions in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
-import { login, logout } from "./store/authStore"
+import { login, logout } from "./store/authSlice"
 import  Header  from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import { Outlet } from 'react-router-dom'
